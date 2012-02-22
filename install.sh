9665d183ade02e2a93ad5cc52aa8d958e453ea23
natxet <nacho@cofre.net>
2012-02-22 10:46:12 +0100

    Don't use wget (mac user don't have it), add gitignore to vendor

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 # This script will install the Opera PHP Framework
+
 mkdir -p httpdocs
 mkdir -p scripts
-echo "Downloading basic framework files"
-wget -q -O httpdocs/.htaccess https://raw.github.com/natxet/OperaFramework/master/httpdocs/.htaccess
-wget -q -O httpdocs/index.php https://raw.github.com/natxet/OperaFramework/master/httpdocs/index.php
-wget -q -O scripts/cli.php https://raw.github.com/natxet/OperaFramework/master/scripts/cli.php
-wget -q -O scripts/post-commit.sh https://raw.github.com/natxet/OperaFramework/master/scripts/post-commit.sh
-wget -q -O composer.json https://raw.github.com/natxet/OperaFramework/master/composer.json
+mkdir -p app
 
-echo "Type the name of your Application and[ENTER]"
-echo " (a folder will be created in ./app/YourApplicationName):"
-read app
-mkdir -p "app/$app"
-echo "Folder created: ./app/$app"
+echo "Downloading basic framework files"
+curl -k -s -o httpdocs/.htaccess https://raw.github.com/natxet/OperaFramework/master/httpdocs/.htaccess
+curl -k -s -o httpdocs/index.php https://raw.github.com/natxet/OperaFramework/master/httpdocs/index.php
+curl -k -s -o scripts/cli.php https://raw.github.com/natxet/OperaFramework/master/scripts/cli.php
+curl -k -s -o scripts/post-commit.sh https://raw.github.com/natxet/OperaFramework/master/scripts/post-commit.sh
+curl -k -s -o composer.json https://raw.github.com/natxet/OperaFramework/master/composer.json
+
+rm -f composer.phar
+rm -f composer.lock
+rm -Rf vendor
 
 echo "Downloading composer for dependencies"
-wget -q -O composer.phar http://getcomposer.org/composer.phar
+curl -s -o composer.phar http://getcomposer.org/composer.phar
 
 echo "Downloading dependencies (libraries) in ./vendor"
 echo "This might take some minutes"
 echo "If you recieve an error, just type:"
 echo " php composer.phar install"
 php composer.phar -q install
+
+curl -k -s -o vendor/.gitignore https://raw.github.com/natxet/OperaFramework/master/vendor/.gitignore
+
