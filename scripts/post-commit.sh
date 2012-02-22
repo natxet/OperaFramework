2531c5966ec50767ba61604e28abc48487a4a996
natxet <nacho@cofre.net>
2012-02-23 00:53:04 +0100

    Cambio a post update

--- scripts/post-commit.sh
+++
@@ -1,9 +0,0 @@
-echo "*** BEGIN pre-commit"
-echo "** Environment $1"
-echo "** App $2"
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-echo "** Executing AssetsParser"
-php $DIR/cli.php \\OperaCore\\Cli\\AssetsParser $1 $2
-echo "*** END pre-commit"
